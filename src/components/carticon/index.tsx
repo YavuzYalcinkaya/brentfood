a6b43c43487d6c38ba839cfc9b054593fc49dff4
Yavuz Yalçınkaya <recepyavuzyalcinkaya@gmail.com>
2023-08-11 17:06:09 +0300

    created zustand

--- src/components/carticon/index.tsx
+++ src/components/carticon/index.tsx
@@ -1,8 +1,16 @@
+"use client";
+import { useCartStore } from "@/utils/store";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 
 const CartIcon = () => {
+  const { totalItems } = useCartStore();
+
+  useEffect(() => {
+    useCartStore.persist.rehydrate();
+  }, []);
+
   return (
     <Link href="/cart" className="flex items-center gap-4">
       <div className="relative w-8 h-8 md:w-5 md:h-5">
@@ -13,7 +21,7 @@ const CartIcon = () => {
           sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 25vw"
         />
       </div>
-      <span>Cart (3)</span>
+      <span>{totalItems}</span>
     </Link>
   );
 };
